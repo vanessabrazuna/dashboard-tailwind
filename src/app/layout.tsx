1fd6482970eba9cc1aad0365d7521711963f4c68
Vanessa Brazuna <brazuna.nessa@gmail.com>
2023-08-18 16:22:15 -0300

    Initial commit - Layout Structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Tailwind Next',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="min-h-screen grid grid-cols-app">
+          <aside className="border-r border-zinc-200 px-5 py-8">sidebar</aside>
+
+          <main className="px-4 pb-12 pt-8">{children}</main>
+        </div>
+      </body>
+    </html>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+
+      colors: {
+        rocketseat: '#8257e6',
+      },
+    },
+  },
+  plugins: [],
+}
